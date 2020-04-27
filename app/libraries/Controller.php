b2a384adc6deb08d5f36cbffee2e3d7856ac88d4
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-27 12:27:11 -0600

    build out base controller to handle models and views

---
+++ app/libraries/Controller.php
@@ -0,0 +1,25 @@
+<?php
+    /*
+     * Base Controller
+     * Load the models and views
+     */
+    class Controller{
+//        Load Model
+        public function model($model){
+//        Require model file
+          require_once "../app/models/" . $model . '.php';
+
+//          Instantiate model that is being passed into the method.
+          return new $model();
+        }
+
+//        Load View
+        public function view($view, $data = []){
+//            Check for view file
+            if(file_exists("../app/views/" . $view . ".php")){
+                require_once "../app/views/" . $view . ".php";
+            }else{
+                die("View Does not exist");
+            }
+        }
+    }
