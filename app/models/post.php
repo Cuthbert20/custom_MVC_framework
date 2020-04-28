a487cf688302e5dfb92f973d7cb4950197139977
Cuthbert20 <knowltonspencer@gmail.com>
2020-04-28 15:27:48 -0600

    finished setting up database, demo model post and displaying with view on index.php. Also added comments to notes.txt

--- app/models/post.php
+++ app/models/post.php
@@ -1,9 +1,16 @@
 <?php
+//    This class ie Post is for demo, testing, and how to.
     class Post{
         private $db;
 
         public function __construct(){
             $this->db = new Database();
+        }
+
+        public function getPosts(){
+            $this->db->query('SELECT * FROM posts');
+
+            return $this->db->resultSet();
 
         }
     }
